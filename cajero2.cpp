220f3f1d59c2ed709a2fecd45c7afe73cc6587ee
Lopez Lasso Cesar Antonio <145069785+Lopezlasso@users.noreply.github.com>
2023-10-27 13:15:04 -0500

    Add files via upload

---
+++ cajero2.cpp
@@ -0,0 +1,24 @@
+//Programa: seguridad 3 capas
+//autor: Lopez Cesar
+//fecha: 05-10-2023
+#include<iostream>
+using namespace std;
+int main()
+{
+   int c11,c12,c13, c01,c02,c03;                         cout<<"Ingrese las contraseñás c1 c2 c3";
+   cin>>c11>>c12>>c13;
+   if(c11==01){
+     if(c12==02){
+     if(c13==03){
+       cout<<"ACCESO AUTORIZADO \n";
+ }else{
+ cout<<"La tercera contraseña no fue correcta"<<endl;
+ }
+ }else{
+ cout<<"La segunda contraseña no fue correcta"<<endl;
+ }
+ }else{
+ cout<<"La primera contraseña no fue correcta"<<endl;
+ }
+ return (0);
+}
